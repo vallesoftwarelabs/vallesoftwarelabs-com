d67dc837e086061a0d4acb6e4a0f52391d51d3ee
Aleksander Valle Grunnvoll <vallegrunnvoll@gmail.com>
2024-06-28 23:39:22 +0200

    Begin implementing design

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,20 @@
+// src/styles/globalStyles.js
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: ${(props) => props.theme.fonts.main};
+    background-color: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.primary};
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  main {
+    padding: ${(props) => props.theme.spacing.padding};
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+// src/styles/theme.js
+export const theme = {
+    colors: {
+        primary: "#000", // Primary color for text
+        background: "#fff", // Background color
+    },
+    fonts: {
+        main: "'Roboto', sans-serif",
+    },
+    spacing: {
+        padding: "1rem",
+        margin: "1rem",
+    },
+};
